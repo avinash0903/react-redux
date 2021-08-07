f3c29a47db55ba53efd77ae16aa5dda980b568da
avinash0903 <avinash.skzr@gmail.com>
2021-08-07 19:21:02 +0530

    1 file modified

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
-import { cretaeStore } from 'redux';
+import { createStore } from 'redux';
 import { reducers } from './reducer/index';
 
-const store = cretaeStore(
+const store = createStore(
   reducers,
   {},
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
